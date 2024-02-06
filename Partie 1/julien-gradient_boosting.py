8f206ee4df47410a9bf5c758e2ff270097432cef
Zeitrom <hugo.verley@gmail.com>
2024-02-06 16:36:07 +0100

    fix bug

--- Partie 1/julien-gradient_boosting.py
+++ Partie 1/julien-gradient_boosting.py
@@ -76,7 +76,7 @@ dt_r2 = r2_score(y_test, dt_prediction)
 print("R2 pour le modèle de Boosting de Gradient:", dt_r2)
 
 # Route pour faire des prédictions via une API REST
-@app.route('/http://127.0.0.1:5000/predict', methods=['GET'])
+@app.route('/predict', methods=['GET'])
 def predict():
     try:
         # Extraction des paramètres de la requête
